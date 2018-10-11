d54f4759ae43a67b9077da03e9d5eca823b24699
danbuckland1 <buckd409@gmail.com>
2018-10-10 20:30:00 -0400

    changed title.

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -4,7 +4,7 @@ import "./Jumbotron.css"
 const Jumbotron = () => (
     <div className="jumbotron jumbotron-fluid">
         <div className="container">
-            <h1 className="display-4">Clicky Game</h1>
+            <h1 className="display-4">Clicky Memory Game</h1>
             <h3>80s Cartoon Edition</h3>
             <p className="lead">Click on each character once to win the game.  If you click on a character twice, the game starts over!</p>
         </div>
